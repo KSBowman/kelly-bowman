648157ed3104e79a49c60e271da5aa7fd066928f
Kelly Bowman <KBowman5@CNM.Edu>
2022-10-18 12:30:57 -0600

    time for TAS and feeling great due to a breakthrough 'timeBasedGreeting'

---
+++ script.js
@@ -0,0 +1,22 @@
+/* this is to change the text in the initial greeting based on the time of day - NEEDS TO GRAB TIME TO USE AS ARGUMENT*/
+let hourOfDay = new Date().getHours();
+
+console.log(hourOfDay)
+
+function timeBasedGreeting (userDeviceHourOfDay) {
+    if (userDeviceHourOfDay >= 4 && userDeviceHourOfDay <= 10) {
+        return "morning";
+    }
+    if (userDeviceHourOfDay >= 11 && userDeviceHourOfDay <= 16) {
+        return "afternoon";
+    }
+    else
+        return "evening";
+}
+/* If chosen to reduce it, but not needed, maybe even hinder */
+let time = (timeBasedGreeting(hourOfDay))
+
+console.log(time)
+
+/* more likely to just use the function name and argument */
+console.log(timeBasedGreeting(hourOfDay))
