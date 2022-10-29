fc77e7b3a2712712b9579a43e1a1879ec68784eb
Kelly Bowman <KBowman5@CNM.Edu>
2022-10-29 02:55:15 -0600

    having issues getting '.skillLogo''s to act as needed.

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-/* this is to change the text in the initial greeting based on the time of day - NEEDS TO GRAB TIME TO USE AS ARGUMENT*/
+// this is to change the text in the initial greeting based on the time of day - NEEDS TO GRAB TIME TO USE AS ARGUMENT
 let hourOfDay = new Date().getHours();
-
 console.log(hourOfDay)
-
 function timeBasedGreeting (userDeviceHourOfDay) {
     if (userDeviceHourOfDay >= 4 && userDeviceHourOfDay <= 10) {
         return "morning";
@@ -18,6 +16,7 @@ let time = (timeBasedGreeting(hourOfDay))
 console.log(time)
 /* more likely to just use the function name and argument */
 console.log(timeBasedGreeting(hourOfDay))
-
 /* had to use below to transfer product of above function into the <span> element section. */
 document.getElementById("hello-span").innerHTML = timeBasedGreeting(hourOfDay)
+
+
