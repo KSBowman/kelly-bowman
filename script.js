195e1ad2501d92fa7a9822a9fe483b02211d8184
Kelly Bowman <KBowman5@CNM.Edu>
2022-11-03 14:58:55 -0600

    ran javascript validator, see doc's (where all lighthouse and validations are held)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // this is to change the text in the initial greeting based on the time of day - NEEDS TO GRAB TIME TO USE AS ARGUMENT
 let hourOfDay = new Date().getHours();
-console.log(hourOfDay)
+console.log(hourOfDay);
 function timeBasedGreeting (userDeviceHourOfDay) {
     if (userDeviceHourOfDay >= 4 && userDeviceHourOfDay <= 10) {
         return "morning";
@@ -13,11 +13,11 @@ function timeBasedGreeting (userDeviceHourOfDay) {
     }
 }
 /* If chosen to reduce it, but not needed, maybe even hinder */
-let time = (timeBasedGreeting(hourOfDay))
-console.log(time)
+let time = (timeBasedGreeting(hourOfDay));
+console.log(time);
 /* more likely to just use the function name and argument */
-console.log(timeBasedGreeting(hourOfDay))
+console.log(timeBasedGreeting(hourOfDay));
 /* had to use below to transfer product of above function into the <span> element section. */
-document.getElementById("hello-span").innerHTML = timeBasedGreeting(hourOfDay)
+document.getElementById("hello-span").innerHTML = timeBasedGreeting(hourOfDay);
 
 
